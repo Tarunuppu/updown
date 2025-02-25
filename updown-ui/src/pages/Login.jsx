56fb5fc0581a00adae226310c156ffaf5bdda3b5
Tarunuppu <uppu.tarunkumar@vmock.com>
2025-02-25 10:29:10 +0530

    Feat(app):having Valid Token, login directs to dashboard

--- updown-ui/src/pages/Login.jsx
+++ updown-ui/src/pages/Login.jsx
@@ -92,6 +92,12 @@ class Login extends Component {
       renderLoginPage: true,
     }
   }
+  componentDidMount() {
+    let accessToken = localStorage.getItem('access_token')
+    if (accessToken) {
+      window.location.href = '/dashboard'
+    }
+  }
 
   handleLogin = (e) => {
     e.preventDefault()
